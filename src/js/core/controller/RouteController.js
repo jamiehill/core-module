83a07e066667318839dbc6521c99c3017331e232
Jamie Hill <jamie@tiltdigital.com>
2015-07-07 17:59:43 +0100

    login/register added

--- src/js/core/controller/RouteController.js
+++ src/js/core/controller/RouteController.js
@@ -46,6 +46,9 @@ export default Marionette.Controller.extend({
 			if (_.isFunction(obj.ctrl[callback])){
 				obj.ctrl[callback].apply(obj.ctrl, _.initial(args));
 			}
+			else if(_.isFunction(obj.ctrl.onNoMatch)) {
+				obj.ctrl.onNoMatch.apply(obj.ctrl, [callback]);
+			}
 
 			// invoke general route handler on controller
 			if (_.isFunction(obj.ctrl.onRouteChange)){
